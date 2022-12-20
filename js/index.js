efaa9024f7c6eadf3b1ae24171ca1947a3797e45
IAOTS <121063496+IAOTS@users.noreply.github.com>
2022-12-20 18:04:46 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
        var btn = document.getElementById("btn");
        btn.onclick = function() {
+                  let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+        // we check if the name is falsey:
+        if (name == "Hanan") {
+            parent.location = 'index1.html';
 
-           parent.location = 'index1.html';
+        } else {
+            parent.location = 'index.html';
+
+        }
+
        }
        var note = document.getElementById("TestNotes");
        note.onclick = function() {

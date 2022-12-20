671e938492e632fc4f3de5075bb32984078ec645
IAOTS <121063496+IAOTS@users.noreply.github.com>
2022-12-20 15:58:46 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
        var btn = document.getElementById("btn");
        btn.onclick = function() {
 
-           parent.location = 'quiz.html/quiz-container';
+           parent.location = 'quiz.html';
        }
        var note = document.getElementById("TestNotes");
        note.onclick = function() {

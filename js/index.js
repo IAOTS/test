e74d44befb308d10f04a2b41fcb448b02bf88eb1
IAOTS <121063496+IAOTS@users.noreply.github.com>
2022-12-28 13:03:37 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
        btn.onclick = function() {
                   let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
         // we check if the name is falsey:
-        if (name == "Infakh.") {
+        if (name == "fa21.") {
             parent.location = 'index1.html';
 
         } else {
